05d9b899d57c28cda18f5775386fb5a20ae3a092
amrutha <321910306057@gitam.in>
2021-10-12 14:09:58 +0530

    #python

--- python coding.py
+++ python coding.py
@@ -1,6 +1,7 @@
 #python coding
 
-![Alt text](https://www.techrepublic.com/a/hub/i/r/2021/02/11/5a8fb3bc-ba6a-4169-8a3b-8fb1750da786/resize/1200x/45a081f2811c6fbc803cccb9ab121062/andrey-suslov.jpg "a title")
+![Alt text](https://cdn.activestate.com/wp-content/uploads/2020/04/python-list-packages.png "a title")
+
 
 
 Python is an interpreted high-level general-purpose programming language. Its design philosophy emphasizes code readability with its use of significant indentation. Its language constructs as well as its object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects.[30]
@@ -11,4 +12,4 @@ Guido van Rossum began working on Python in the late 1980s, as a successor to th
 
 Python is dynamically-typed and garbage-collected. It supports multiple programming paradigms, including structured (particularly, procedural), object-oriented and functional programming. It is often described as a "batteries included" language due to its comprehensive standard library.[31]
 
-Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.[32] Python 2.0 was released in 2000 and introduced new features, such as list comprehensions and a garbage collection system using reference counting. Python 3.0 was released in 2008 and was a major revision of the language that is not completely backward-compatible. Python 2 was discontinued with version 2.7.18 in 2020.[33]
+Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.[32] Python 2.0 was released in 2000 and introduced new features, such as list comprehensions and a garbage collection https://www.techrepublic.com/a/hub/i/r/2021/02/11/5a8fb3bc-ba6a-4169-8a3b-8fb1750da786/resize/1200x/45a081f2811c6fbc803cccb9ab121062/andrey-suslov.jpghttps://www.techrepublic.com/a/hub/i/r/2021/02/11/5a8fb3bc-ba6a-4169-8a3b-8fb1750da786/resize/1200x/45a081f2811c6fbc803cccb9ab121062/andrey-suslov.jpg
